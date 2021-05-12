e449c9ada5374b17a20c5c1e0526f1487c1e7e95
c60603 <c60603@@gmail.com>
2021-05-12 15:11:27 +0800

    Initial Commit

---
+++ HW3_Word/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3_Word
+//
+//  Created by User08 on 2021/5/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW3_Word/HW3_WordApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_WordApp.swift
+//  HW3_Word
+//
+//  Created by User08 on 2021/5/12.
+//
+
+import SwiftUI
+
+@main
+struct HW3_WordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
